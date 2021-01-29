530aa1a8a57385435890757cdf485d7ef84f0e14
Ale-Shadow-02 <dvoryadkiin@yandex.ru>
2021-01-29 18:34:24 +0400

    Сделан адаптив главных блоков на 768px

--- project-alve/js/script.js
+++ project-alve/js/script.js
@@ -26,38 +26,27 @@ $(document).ready(function () {
   });
 });
 
-/* var nav = document.getElementById("nav");
-nav.addEventListener("click", function (e) {
-  var target = e.target;
-
-  var targetParent = target.closest(".menu-item");
-
-  if (targetParent) {
-    var subm = targetParent.getElementsByClassName("submenu")[0];
-    close();
-    if (subm) {
-      subm.style.display = "block";
-    }
-  }
-});
-
-function close() {
-  var s = document.getElementsByClassName("submenu");
-  for (var i = 0; i < s.length; i++) {
-    s[i].style.display = "none";
-  }
-} */
 // Выпадающее меню сайдиара
 
 let linkClick = document.querySelector(".navbar__list--link-dropdown");
 let subMenu = document.querySelector(".navbar__submenu--list");
-console.log(subMenu);
 linkClick.addEventListener("click", function (e) {
   e.preventDefault();
   let target = e.target;
-  console.log(target);
   if (target) {
     subMenu.classList.toggle("active");
+  }
+});
+
+// Раскрытие таба data
+
+let arrowLink = document.querySelector(".bid__reqvuisition--arrow");
+let data = document.querySelector(".data");
+arrowLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  let target = e.target;
+  if (target) {
+    data.classList.toggle("active-data");
   }
 });
 ;
